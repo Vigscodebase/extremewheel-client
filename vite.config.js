936f825f7d41bc6ea7f3348e955da29df2275ffd
Vigscodebase <iyerv21@gmail.com>
2026-07-08 11:00:35 +0000

    initial-commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      // Any request to /api in React is silently forwarded to Express
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true, // Helpful for production debugging
+  }
+});
